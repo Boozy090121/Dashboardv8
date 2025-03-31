c3aac0e9bd98be59821b25280e3b8c4c2fa378b4
Boozy090121 <132027776+Boozy090121@users.noreply.github.com>
2025-03-31 18:58:38 -0400

    Add files via upload

---
+++ app-component.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import NovoNordiskDashboard from './novo-nordisk-dashboard'; // Temporarily comment out
+import { DataProvider } from './DataContext.js';
+
+const App: React.FC = () => {
+  return (
+    <DataProvider>
+      {/* <NovoNordiskDashboard /> */}
+      {/* Temporarily render a simple div instead */}
+      <div style={{ padding: '20px' }}>Testing DataProvider... Look at console.</div>
+    </DataProvider>
+  );
+};
+
+export default App;
