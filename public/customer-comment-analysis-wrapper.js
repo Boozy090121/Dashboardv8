27eebf21580170570d0a1c4c25d908bce49637fc
Boozy090121 <132027776+Boozy090121@users.noreply.github.com>
2025-03-31 15:13:05 -0400

    Add files via upload

---
+++ public/customer-comment-analysis-wrapper.js
@@ -0,0 +1,4 @@
+import CustomerCommentAnalysisComponent from './customer-comment-analysis.tsx';
+
+// Re-export the component from the TypeScript file
+export default CustomerCommentAnalysisComponent; 
