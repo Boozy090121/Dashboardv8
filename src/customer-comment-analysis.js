04a8b64ee5414aad2306eab9061dcd8ad922cf22
Boozy090121 <132027776+Boozy090121@users.noreply.github.com>
2025-03-31 16:07:42 -0400

    Add files via upload

---
+++ src/customer-comment-analysis.js
@@ -0,0 +1,3 @@
+// Re-export the component from the wrapper file
+export { CustomerCommentAnalysis } from './customer-comment-analysis-wrapper.js'; 
+export { CustomerCommentAnalysis as default } from './customer-comment-analysis-wrapper.js';
